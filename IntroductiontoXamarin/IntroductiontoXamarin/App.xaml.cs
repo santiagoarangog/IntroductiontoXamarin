763358b51c4cfcaf30fbff3d597f6df71a4993a6
Santiago Arango Gutierrez <santiago9606@gmail.com>
2022-07-05 10:28:52 -0500

    Modify home page

--- IntroductiontoXamarin/IntroductiontoXamarin/App.xaml.cs
+++ IntroductiontoXamarin/IntroductiontoXamarin/App.xaml.cs
@@ -10,7 +10,7 @@ namespace IntroductiontoXamarin
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Views.MainPage());
         }
 
         protected override void OnStart ()
@@ -18,7 +18,7 @@ namespace IntroductiontoXamarin
         }
 
         protected override void OnSleep ()
-        {
+        { 
         }
 
         protected override void OnResume ()
--- IntroductiontoXamarin/IntroductiontoXamarin/Views/MainPage.xaml.cs
+++ IntroductiontoXamarin/IntroductiontoXamarin/Views/MainPage.xaml.cs
@@ -6,13 +6,51 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace IntroductiontoXamarin
+namespace IntroductiontoXamarin.Views
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        // Seteamos nuestras variables
+        double N1, N2, Resultado;
+
+        public void btnHome_Clicked(System.Object sender,System.EventArgs e)
+        {
+            DisplayAlert("Titulo de la alerta","Descripcion de la alerta","Ok");
+        }
+
+        public void btnCalcular_Clicked(Object sender, EventArgs e)
+        {
+            this.calcular();
+        }
+
+        private void calcular()
+        {
+            if (!string.IsNullOrEmpty(txtNumeroUno.Text))
+            {
+                if (!string.IsNullOrEmpty(txtNumeroDos.Text))
+                {
+                    N1 = Convert.ToDouble(txtNumeroUno.Text);
+                    N2 = Convert.ToDouble(txtNumeroDos.Text);
+
+                    Resultado = N1 * N2;
+
+                    DisplayAlert("El resultado es ", Resultado.ToString(), "Ok");
+                }
+                else
+                {
+                    DisplayAlert("Error", "La informacion es obligatoria en el campo Numero Dos", "Ok");
+                }
+
+            }
+            else
+            {
+                DisplayAlert("Error", "La informacion es obligatoria en el campo Numero Uno", "Ok");
+            }
         }
     }
 }
